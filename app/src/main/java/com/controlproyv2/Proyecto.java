25106f0b7248fc7bee67629029ade8faf4674ee1
neckroangel@gmail.com <Velkyo19??>
2022-11-26 23:05:27 -0300

    asd

---
+++ app/src/main/java/com/controlproyv2/Proyecto.java
@@ -0,0 +1,13 @@
+package com.controlproyv2;
+
+public class Proyecto {
+
+    protected String id;
+    protected String nombre;
+    protected String apellido;
+    protected String edad;
+    protected String Duracion;
+    protected String Estrellas;
+
+
+}
